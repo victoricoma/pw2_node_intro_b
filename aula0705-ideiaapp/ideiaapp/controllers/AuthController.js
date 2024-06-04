47d18edc8b9289da8ef6324b1321e9b622004e69
victoricoma <victor.icoma@etec.sp.gov>
2024-06-04 20:37:00 -0300

    Aula0406 estruturação de projeto para deploy e conector Azure

--- aula0705-ideiaapp/ideiaapp/controllers/AuthController.js
+++
@@ -1,8 +0,0 @@
-const User = require('../models/User')
-const bcrypt = require('bcryptjs')
-
-module.exports = class UserController{
-    static login(req, res){
-        res.render('auth/login')
-    }
-}
